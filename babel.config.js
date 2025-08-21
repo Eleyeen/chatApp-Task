3b0c8b600788a58846e832a20eeae7a735305ef4
Eleyeen Awan <46245561+Eleyeen@users.noreply.github.com>
2025-08-21 14:25:59 +0500

    init

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  const isTest = api.env('test');
-  return {
-    presets: ['module:@react-native/babel-preset'],
-    plugins: [
-      'react-native-worklets/plugin',
-    ],
-  };
-};
